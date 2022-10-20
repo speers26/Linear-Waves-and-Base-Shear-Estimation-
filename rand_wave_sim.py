3e35ea987087c31e59cf2d745eb820d641fb3880
Matthew Speers <mattspeers33@gmail.com>
2022-10-20 13:21:23 +0100

    added density function

--- rand_wave_sim.py
+++ rand_wave_sim.py
@@ -1,3 +1,21 @@
+from math import e
 import numpy as np
 from scipy import optimize
 import matplotlib.pyplot as plt
+
+def d_jonswap(f, hs, tp):
+    """
+    returns JONSWAP density for given frequency 
+
+    Args:
+        f (_type_): frequency [s^-1]
+        hs (_type_): significant wave height [m]
+        tp (_type_): significant wave period [s]
+    """
+
+    omega = 2 * np.pi * f
+    omega_p = 2 * np.pi / tp
+
+    dens = 5 / 16 * hs ** 2 * omega ** -5 * e ** (-5 / 4 * (omega / omega_p) ** -4)
+
+    return dens
