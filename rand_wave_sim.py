794cc6947d661fa786c347597fe5596c82761683
Matthew Speers <mattspeers33@gmail.com>
2022-10-27 14:55:34 +0100

    fixing jonswap density

--- rand_wave_sim.py
+++ rand_wave_sim.py
@@ -4,7 +4,7 @@ from scipy import optimize
 import matplotlib.pyplot as plt
 import stringari_jonswap as sj
 
-def jonswap(f, hs, tp):
+def djonswap(f, hs, tp):
     """
     returns JONSWAP density for given frequency 
 
@@ -13,32 +13,24 @@ def jonswap(f, hs, tp):
         hs (_type_): significant wave height [m]
         tp (_type_): significant wave period [s]
     """
-
+    g = 9.81
     fp = 1. / tp
 
-    ## from holth book
-    gamma = 5.87 * fp ** 0.86
-    sigma_a = 0.0547 * fp ** 0.32
-    sigma_b = 0.0783 * fp ** 0.16
-
-    ##alpha = 1- 0.287 * np.log(gamma)## from "DNV-RP-C205 Environmental Conditions and Environmental Loads"
-    ##alpha from yamaguchi - ?? who is this (got this from code i found)
-    alpha = 1. / (.06533 * gamma ** .8015 + .13467) / 16.
-
-    dens = alpha * hs ** 2 * tp ** -4 * f ** -5 * np.exp(-1.25 * (tp*f) ** -4)
+    ## from https://wikiwaves.org/Ocean-Wave_Spectra
+    gamma = 3.3
+    sigma_a = 0.07 
+    sigma_b = 0.09
 
     if f <= fp:
         sigma = sigma_a
     else :
         sigma = sigma_b
 
-    norm_dens = dens * gamma ** np.exp(-0.5 * (tp * f - 1) ** 2 / sigma**2) #/ ( hs ** 2 ) * 4
+    gamma_coeff = gamma ** np.exp(-0.5 * (((f / fp - 1)/sigma) ** 2)) 
+    unn_dens = g ** 2 * (2 * np.pi) ** -4 * f ** -5 * np.exp(-1.25 * (tp*f) ** -4) * gamma_coeff
+    n_dens = unn_dens
 
-    return norm_dens
-
-def d_jonswap(f, hs, tp):
-    density = jonswap(f,hs,tp) / ( hs ** 2 ) * 4
-    return density
+    return n_dens
 
 def random_waves_surface(t): 
     eta = 0
@@ -50,8 +42,18 @@ if __name__ == "__main__":
     tp = 20
     f_p = 1/tp
 
-    n_freq = 1000
+    n_freq = 500
     f_seq = np.linspace(1e-3,f_p*5,n_freq)
-    
-    n_time = 200
-    time = np.linspace(-20, 20, 200)
+    df = f_seq[1] - f_seq[0]
+
+    dens = np.empty(n_freq)
+
+    for i_f, f in enumerate(f_seq):
+        dens[i_f] = djonswap(f,hs,tp)
+
+    alpha = df * sum(dens)
+    dens = dens / alpha
+
+    plt.figure()
+    plt.plot(f_seq,dens)
+    plt.show()
