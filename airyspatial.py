9ac21d12b54c7c1a4b7379350bdf9a17c4e9705c
Matt <m.speers@lancaster.ac.uk>
2022-11-13 17:56:05 +0000

    airy 3d done

--- airyspatial.py
+++ airyspatial.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator
+
 
 import airy as a1
 
@@ -10,12 +9,10 @@ def airy_wave_surface(k, A, xrange, yrange, omega, t, theta):
     kx = k * np.cos(theta)
     ky = k * np.sin(theta)
 
-    kxky = [(kx * x, ky * y) for x in xrange for y in yrange]
-    kxx = np.array([x for (x,y) in kxky])
-    kyy = np.array([y for (x,y) in kxky])
+    xpoints, ypoints = np.meshgrid(xrange, yrange)
 
-    omega = np.tile(omega, numx * numy)
-    eta = A * np.sin(omega * t - kxx - kyy)
+    omega = np.tile(omega, numx * numy).reshape(numx, numy)
+    eta = A * np.sin(omega * t - kx * xpoints - ky * ypoints)
 
     eta.shape = (numx, numy)
     
@@ -27,24 +24,27 @@ if __name__ == '__main__':
     h = 100
     T = 20
     A = 35/2
-    theta = np.pi /4
+    theta = np.pi/4
 
     numx = 30
     numy = 30
 
-    xrange = np.linspace(-5, 5, numx)
-    yrange = np.linspace(-5, 5, numy)
-    t = 0
+    xrange = np.linspace(-500, 500, numx)
+    yrange = np.linspace(-500, 500, numy)
+    t = 10
 
     k, omega = a1.airy_dispersion(h, T)
 
     eta = airy_wave_surface(k, A, xrange, yrange, omega, t, theta)
-    print(eta)
+
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
     X, Y = np.meshgrid(xrange, yrange)
     surf = ax.plot_surface(X, Y, eta)
 
-    ax.set_zlim(-1.5,1.5)
+    
+
+   # plt.contourf(xrange, yrange, eta)
+
 
     plt.show()
