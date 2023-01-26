40ea2ff247ab733b001a4fffe5888dbdabbf1475
Matt <m.speers@lancaster.ac.uk>
2023-01-26 23:20:05 +0000

    added notebook versions of main strips

--- crest_dist_est.py
+++ crest_dist_est.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import fft_rand_wave_sim as rwave  # for random wave sim
+import rand_wave_sim as rwave  # for random wave sim
 
 def djonswap(f, hs, tp):
 
