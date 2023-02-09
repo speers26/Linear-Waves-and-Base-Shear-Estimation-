0d108ae9927834b72dabd85b5c7dfdb39d5038b9
Matthew Speers <mattspeers33@gmail.com>
2023-02-09 17:53:28 +0000

    tidied code

--- force_dist_est.py
+++ force_dist_est.py
@@ -9,12 +9,14 @@ if __name__ == "__main__":
     write = True
     write_con = True
 
+    # set up wave conditions
     hs = 25
     tp = 12
     depth = 100
     cond = False
     a = 0
 
+    # set up arrays
     z_num = 150
     z_range = np.linspace(-depth, 50, z_num)
     dz = z_range[1] - z_range[0]
@@ -38,11 +40,13 @@ if __name__ == "__main__":
     CoH = np.linspace(1e-3, 1.5)
     crest_cdf = wave.rayleigh_cdf(CoH * hs, hs)
 
-    # get jonswap densities
+    # get jonswap density
     jnswp_dens = wave.djonswap(f_range, hs, tp)
 
+    # if we want new wave data
     if write:
 
+        # set up arrays (don't actually need to do this for all)
         eta = np.empty(int(t_num*num_sea_states))
         u_x = np.empty((int(t_num*num_sea_states), z_num))
         u_z = np.empty((t_num, z_num))
@@ -50,107 +54,112 @@ if __name__ == "__main__":
         du_z = np.empty((t_num, z_num))
         F = np.empty((int(t_num*num_sea_states), z_num))
 
+        # populate arrays
         for i in range(num_sea_states):
             print(i)
             eta[i*t_num:(i+1)*t_num], u_x, u_z, du_x, du_z = wave.fft_random_wave_sim(z_range, depth, a, om_range, jnswp_dens, cond)
             for i_t, t in enumerate(t_range):
                 for i_z, z in enumerate(z_range):
-                    # eta[i_t], u_x[i_t, i_z], u_z[i_t, i_z], du_x[i_t, i_z], du_z[i_t, i_z] = wave.ptws_random_wave_sim(t=t, z=z, depth=depth, a=a, om_range=om_range, spctrl_dens=jnswp_dens, cond=cond)
                     F[i_t + i * t_num, i_z] = wave.morison_load(u_x[i_t, i_z], du_x[i_t, i_z])
-
         base_shear = np.sum(F, axis=1) * dz / 1e6  # 1e6 converts to MN from N
+
+        # write data to text files
         np.savetxt('load.txt', base_shear, delimiter=' ')
         np.savetxt('eta.txt', eta, delimiter=' ')
 
+    # if we want to use the old wave data
     else:
 
+        # read data from the text files
         eta = np.loadtxt('eta.txt')
         base_shear = np.loadtxt('load.txt')
 
+    # extend the time range to include all sea states
     new_t_range = np.linspace(0, period * num_sea_states, int(nT*num_sea_states))
 
-    plt.figure()
-    plt.subplot(2, 1, 1)
-    plt.plot(new_t_range, base_shear, '-k')
-    plt.ylabel('Force [MN]')
-    plt.xlabel('Time')
-
+    # get maximum force in each sea state
     max_forces = np.empty(num_sea_states)
     max_ind = np.empty(num_sea_states)
-
     for i_s in range(num_sea_states):
         slice = base_shear[i_s*t_num:t_num*(i_s+1)]
         max_forces[i_s] = max(slice)
         slice_ind = np.where(slice == max(slice))[0]
         max_ind[i_s] = int(i_s*t_num + slice_ind)
 
-    colors = np.tile('#FF0000', num_sea_states)
-
-    plt.scatter(max_ind, max_forces, c=colors)
-    plt.subplot(2, 1, 2)
-
+    # get empirical distribution of max forces
     long_emp = np.empty(num_sea_states)
     s_max_forces_0 = np.sort(max_forces)
     for i_f, f in enumerate(s_max_forces_0):
         long_emp[i_f] = sum(max_forces < f)/num_sea_states
 
-    plt.plot(s_max_forces_0, long_emp, '-k')
-    #plt.show()
+    # set colors for scatter plots
+    colors = np.tile('#FF0000', num_sea_states)
 
+    # plot max forces ts and emp distribution
     plt.figure()
     plt.subplot(2, 1, 1)
-    plt.plot(new_t_range, eta, '-k')
-    plt.ylabel('Crest Height [m]')
+    plt.plot(new_t_range, base_shear, '-k')
+    plt.ylabel('Force [MN]')
     plt.xlabel('Time')
+    plt.scatter(max_ind, max_forces, c=colors)
+    plt.subplot(2, 1, 2)
+    plt.plot(s_max_forces_0, long_emp, '-k')
 
+    # get max crest from each sea state
     max_crests = np.empty(num_sea_states)
-
     for i_s in range(num_sea_states):
         slice = eta[i_s*t_num:t_num*(i_s+1)]
         max_crests[i_s] = max(slice)
         slice_ind = np.where(slice == max(slice))[0]
         max_ind[i_s] = int(i_s*t_num + slice_ind)
 
-    plt.scatter(max_ind, max_crests, c=colors)
-    plt.subplot(2, 1, 2)
-
+    # get max crest height empirical distribution
     long_crest_emp = np.empty(num_sea_states)
     s_max_crests_0 = np.sort(max_crests)
     for i_c, c in enumerate(s_max_crests_0):
         long_crest_emp[i_c] = sum(max_crests < c)/num_sea_states
 
-    plt.plot(s_max_crests_0, long_crest_emp, '-k')
-
+    # get true crest height distribution (rayleigh)
     true_crest_dist = wave.rayleigh_cdf(s_max_crests_0, hs)**waves_per_state
-    plt.plot(s_max_crests_0, true_crest_dist, '--g')
 
-    #plt.show()
+    # plot crest height ts and emp distribution with rayleigh
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    plt.plot(new_t_range, eta, '-k')
+    plt.ylabel('Crest Height [m]')
+    plt.xlabel('Time')
+    plt.scatter(max_ind, max_crests, c=colors)
+    plt.subplot(2, 1, 2)
+    plt.plot(s_max_crests_0, long_crest_emp, '-k')
+    plt.plot(s_max_crests_0, true_crest_dist, '--g')
 
     # now do for conditional sim / IS method
     cond = True
-    # num_sea_states = 10000
-    # max_forces = np.empty(num_sea_states)
-    # max_ind = np.empty(num_sea_states)
-    # colors = np.tile('#FF0000', num_sea_states)
 
+    # get proposal sample and density of crest
     CoHmin = 0
     CoHmax = 2
     CoHnum = num_sea_states
     CoH = np.random.uniform(low=CoHmin, high=CoHmax, size=CoHnum)
     g = 1/(CoHmax-CoHmin)
 
-    sea_state_minutes = 2  # will simulate sea states of 2 minutes
-    period = 60*sea_state_minutes  
+    # will simulate sea states of 2 minutes
+    sea_state_minutes = 2  
+    period = 60*sea_state_minutes
     waves_per_state = period/tp
     sims_per_state = sea_state_hours * 60 / 2
 
+    # get true density and weights
     c = CoH * hs
     f_0 = wave.rayleigh_pdf(CoH*hs, hs)
+    fog = f_0/g
+
+    # ----- do this if true density needs changing from regular rayleigh (not sure if it does or not) ----- #
     # f_prime = np.exp(16/hs**2 - (16*c/hs**2)**2)
     # f = waves_per_state * f_0**(waves_per_state-1) * f_prime
     # fog = f / g
-    fog = f_0/g
 
+    # redo arrays
     freq = 1.00  # number of sample points per second
     nT = np.floor(period*freq)  # number of time points to evaluate
     t_num = int(nT)  # to work with rest of the code
@@ -161,6 +170,7 @@ if __name__ == "__main__":
     f_range = np.linspace(1e-3, nT - 1, int(nT)) / (nT / freq)  # selecting frequency range from 0 to freq
     om_range = f_range * (2*np.pi)
 
+    # redo jonswap density
     jnswp_dens = wave.djonswap(f_range, hs, tp)
 
     if write_con:
@@ -178,7 +188,6 @@ if __name__ == "__main__":
             eta[i*t_num:(i+1)*t_num], u_x, u_z, du_x, du_z = wave.fft_random_wave_sim(z_range, depth, a, om_range, jnswp_dens, cond)
             for i_t, t in enumerate(t_range):
                 for i_z, z in enumerate(z_range):
-                    # eta[i_t], u_x[i_t, i_z], u_z[i_t, i_z], du_x[i_t, i_z], du_z[i_t, i_z] = wave.ptws_random_wave_sim(t=t, z=z, depth=depth, a=a, om_range=om_range, spctrl_dens=jnswp_dens, cond=cond)
                     F[i_t + i * t_num, i_z] = wave.morison_load(u_x[i_t, i_z], du_x[i_t, i_z])
 
         base_shear = np.sum(F, axis=1) * dz / 1e6  # 1e6 converts to MN from N
@@ -192,61 +201,55 @@ if __name__ == "__main__":
 
     new_t_range = np.linspace(0, period * num_sea_states, int(nT*num_sea_states))
 
-    plt.figure()
-    plt.subplot(2, 1, 1)
-    plt.plot(new_t_range, base_shear, '-k')
-    plt.ylabel('Force [MN]')
-    plt.xlabel('Time')
-
+    # get max forces
     for i_s in range(num_sea_states):
         slice = base_shear[i_s*t_num:t_num*(i_s+1)]
         max_forces[i_s] = max(slice)
         slice_ind = np.where(slice == max(slice))[0]
         max_ind[i_s] = int(i_s*t_num + slice_ind)
 
+    # get IS max force distribution
     s_max_forces = np.sort(max_forces)
     is_0 = np.empty(len(s_max_forces))
     for i_f, f in enumerate(s_max_forces):
         is_0[i_f] = sum((max_forces < f)*(fog))/sum(fog)
-
     is_1 = is_0**sims_per_state
 
-    plt.scatter(max_ind, max_forces, c=colors)
-    plt.subplot(2, 1, 2)
-
-    plt.plot(s_max_forces, np.log10(1-is_1), '-b')
-    plt.plot(s_max_forces_0, np.log10(1-long_emp), '-r')
-    #plt.show()
-
+    # plot max forces and comparison of empirical and IS force dists
     plt.figure()
     plt.subplot(2, 1, 1)
-    plt.plot(new_t_range, eta, '-k')
-    plt.ylabel('Crest Height [m]')
+    plt.plot(new_t_range, base_shear, '-k')
+    plt.ylabel('Force [MN]')
     plt.xlabel('Time')
+    plt.scatter(max_ind, max_forces, c=colors)
+    plt.subplot(2, 1, 2)
+    plt.plot(s_max_forces, np.log10(1-is_1), '-b')
+    plt.plot(s_max_forces_0, np.log10(1-long_emp), '-r')
   
+    # get max crests
     max_crests = np.empty(num_sea_states)
-
     for i_s in range(num_sea_states):
         slice = eta[i_s*t_num:t_num*(i_s+1)]
         max_crests[i_s] = max(slice)
         slice_ind = np.where(slice == max(slice))[0]
         max_ind[i_s] = int(i_s*t_num + slice_ind)
 
-    plt.scatter(max_ind, max_crests, c=colors)
-    plt.subplot(2, 1, 2)
-
+    # get IS crest distribution
     is_crest_dist = np.empty(num_sea_states)
     s_max_crests = np.sort(max_crests)
     for i_c, c in enumerate(s_max_crests):
         is_crest_dist[i_c] = sum((max_crests < c)*(fog))/sum(fog)
-
     is_crest_dist_1 = is_crest_dist**sims_per_state
 
+    # plot max crests and emp crest, true crest and IS crest distributions
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    plt.plot(new_t_range, eta, '-k')
+    plt.ylabel('Crest Height [m]')
+    plt.xlabel('Time')
+    plt.scatter(max_ind, max_crests, c=colors)
+    plt.subplot(2, 1, 2)
     plt.plot(s_max_crests_0, np.log10(1-long_crest_emp), '-r')
-
-    #true_crest_dist = wave.rayleigh_cdf(s_max_crests, hs)
     plt.plot(s_max_crests_0, np.log10(1-true_crest_dist), '--g')
-
     plt.plot(s_max_crests, np.log10(1-is_crest_dist_1), '-b')
-
     plt.show()
