edb83c3609dc249fc41b7387133993bc56e7fc7a
Matthew Speers <mattspeers33@gmail.com>
2023-02-21 12:53:51 +0000

    temporalily removed all but eta sim

--- force_dist_est.py
+++ force_dist_est.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     period = 60**2 * sea_state_hours  # total time range in seconds
     waves_per_state = period/tp
 
-    freq = 1.00  # number of sample points per second
+    freq = 4.00  # number of sample points per second
     nT = np.floor(period*freq)  # number of time points to evaluate
     t_num = int(nT)  # to work with rest of the code
 
@@ -42,33 +42,19 @@ if __name__ == "__main__":
     # if we want new wave data
     if write:
         # set up arrays (don't actually need to do this for all)
-        eta = np.empty(int(t_num*num_sea_states))
-        u_x = np.empty((int(t_num*num_sea_states), z_num))
-        u_z = np.empty((t_num, z_num))
-        du_x = np.empty((t_num, z_num))
-        du_z = np.empty((t_num, z_num))
-        F = np.empty((int(t_num*num_sea_states), z_num))
+        eta = np.empty([num_sea_states, t_num])
 
         # populate arrays
         for i in range(num_sea_states):
             print(i)
-            eta[i*t_num:(i+1)*t_num], u_x, u_z, du_x, du_z = wave.fft_random_wave_sim(z_range, depth, a, om_range, jnswp_dens, cond)
-            for i_t, t in enumerate(t_range):
-                for i_z, z in enumerate(z_range):
-                    F[i_t + i * t_num, i_z] = wave.morison_load(u_x[i_t, i_z], du_x[i_t, i_z])
-        base_shear = np.sum(F, axis=1) * dz / 1e6  # 1e6 converts to MN from N
+            eta[i, :], _, _, _, _ = wave.fft_random_wave_sim(z_range, depth, a, om_range, jnswp_dens, cond)
 
-        # write data to text files
-        np.savetxt('load.txt', base_shear, delimiter=' ')
         np.savetxt('eta.txt', eta, delimiter=' ')
-        # np.savetxt('h_velocity.txt', u_x, delimiter=' ')
 
     # if we want to use the old wave data
     else:
         # read data from the text files
         eta = np.loadtxt('eta.txt')
-        base_shear = np.loadtxt('load.txt')
-        # u_x = np.loadtxt('h_velocity.txt')
 
     # extend the time range to include all sea states
     new_t_range = np.linspace(0, period * num_sea_states, int(nT*num_sea_states))
@@ -106,11 +92,20 @@ if __name__ == "__main__":
     f = wave.rayleigh_pdf(r_crests, hs)
     fog = f/g
 
+    x = np.linspace(0, 2*hs, num=100)
+    cdf_crest = np.empty(x.shape)
+    for i_x, c in enumerate(x):
+        cdf_crest[i_x] = np.sum((r_crests<c) * fog)/np.sum(fog)
+
+    cdf_crest_ss_max = cdf_crest**(waves_per_state*sims_per_state)
+    rayleigh_cdf = wave.rayleigh_cdf(x, hs)
+    rayleigh_ss_max_cdf = rayleigh_cdf**(waves_per_state*sims_per_state)
+
     plt.figure()
     plt.plot(r_crests, f)
 
     # redo arrays
-    freq = 1.00  # number of sample points per second
+    freq = 4.00  # number of sample points per second
     nT = np.floor(period*freq)  # number of time points to evaluate
     t_num = int(nT)  # to work with rest of the code
 
@@ -126,30 +121,17 @@ if __name__ == "__main__":
     if write_con:
         # generate wave data and write to text files
         eta = np.empty(int(t_num*num_sea_states))
-        u_x = np.empty((t_num, z_num))
-        u_z = np.empty((t_num, z_num))
-        du_x = np.empty((t_num, z_num))
-        du_z = np.empty((t_num, z_num))
-        F = np.empty((int(t_num*num_sea_states), z_num))
-
         for i in range(num_sea_states):
             print(i)
             a = CoH[i] * hs
-            eta[i*t_num:(i+1)*t_num], u_x, u_z, du_x, du_z = wave.fft_random_wave_sim(z_range, depth, a, om_range, jnswp_dens, cond)
-            for i_t, t in enumerate(t_range):
-                for i_z, z in enumerate(z_range):
-                    F[i_t + i * t_num, i_z] = wave.morison_load(u_x[i_t, i_z], du_x[i_t, i_z])
+            eta[i*t_num:(i+1)*t_num], _, _, _, _ = wave.fft_random_wave_sim(z_range, depth, a, om_range, jnswp_dens, cond)
 
-        base_shear = np.sum(F, axis=1) * dz / 1e6  # 1e6 converts to MN from N
-        np.savetxt('load_con.txt', base_shear, delimiter=' ')
         np.savetxt('eta_con.txt', eta, delimiter=' ')
         # np.savetxt('h_v_con.txt', u_x, delimiter=' ')
 
     else:
         # read wave date from txt files
-        base_shear = np.loadtxt('load_con.txt')
         eta = np.loadtxt('eta_con.txt')
-        # u_x = np.loadtxt('h_v_con.txt')
 
     new_t_range = np.linspace(0, period * num_sea_states, int(nT*num_sea_states))
 
@@ -167,12 +149,41 @@ if __name__ == "__main__":
         max_ind[i_s] = int(i_s*t_num + slice_ind)
 
     # get IS crest distribution
-    is_crest_dist = np.empty(num_sea_states)
-    for i_c, c in enumerate(r_crests):
+    is_crest_dist = np.empty(x.shape)
+    for i_c, c in enumerate(x):
         is_crest_dist[i_c] = np.sum((max_crests < c) * fog)/np.sum(fog)
     is_crest_dist_1 = is_crest_dist**sims_per_state
 
     colors = np.tile('#FF0000', num_sea_states)
+
+    cdf_crest_two_min = cdf_crest**waves_per_state
+    cdf_rayleigh_ss_two_min = rayleigh_cdf**waves_per_state
+
+    plt.figure()
+    plt.plot(r_crests, np.sort(max_crests), '.')
+    plt.plot([0,50], [0,50], 'k')
+    plt.show()
+
+    plt.figure()
+    plt.plot(t_range, eta[np.argmax(max_crests)*t_num:(np.argmax(max_crests)+1)*t_num])
+    plt.show()
+
+    plt.figure()
+    plt.subplot(1,3,1)
+    plt.plot(x/hs, np.log10(1-cdf_crest))
+    plt.plot(x/hs, np.log10(1-rayleigh_cdf), '-r')
+
+    plt.subplot(1,3,2)
+    plt.plot(x/hs, np.log10(1-cdf_crest_two_min))
+    plt.plot(x/hs, np.log10(1-cdf_rayleigh_ss_two_min), '-r')
+    plt.plot(x/hs, np.log10(1-is_crest_dist), '--g')
+
+    plt.subplot(1,3,3)
+    plt.plot(x/hs, np.log10(1-cdf_crest_ss_max))
+    plt.plot(x/hs, np.log10(1-rayleigh_ss_max_cdf), '-r')
+    plt.plot(x/hs, np.log10(1-is_crest_dist_1), '--g')
+    plt.show()
+
 
     # plot max crests and emp crest, true crest and IS crest distributions
     plt.figure()
