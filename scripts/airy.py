82688661f74317699a047d618771230a73982914
Matthew Speers <mattspeers33@gmail.com>
2023-03-15 19:39:25 +0000

    setting up classe

--- scripts/airy.py
+++ scripts/airy.py
@@ -23,15 +23,15 @@ if __name__ == '__main__':
     dw = np.empty((n_time, n_depth))
     F = np.empty((n_time, n_depth))
 
-    k, omega = airy_dispersion(h, T)
+    airy_wave = kin.AiryKin(times=time, z_values=z_range, H=A*2, T=T)
 
-    for i_t, t in enumerate(time):
-        for i_z, z in enumerate(z_range):
-            eta[i_t], u[i_t, i_z], w[i_t, i_z], du[i_t, i_z], dw[i_t, i_z] = airy_kinematics(k, h, A, x, omega, t, z)
+    # for i_t, t in enumerate(time):
+    #     for i_z, z in enumerate(z_range):
+    #         eta[i_t], u[i_t, i_z], w[i_t, i_z], du[i_t, i_z], dw[i_t, i_z] = airy_kinematics(k, h, A, x, omega, t, z)
 
-            F[i_t, i_z] = morison_load(u[i_t, i_z], du[i_t, i_z])
+    #         F[i_t, i_z] = morison_load(u[i_t, i_z], du[i_t, i_z])
 
-    base_shear = np.sum(F, axis=1) * dz / 1e6  # 1e6 converts to MN from N
+    # base_shear = np.sum(F, axis=1) * dz / 1e6  # 1e6 converts to MN from N
 
     plt.figure()
     plt.subplot(2, 2, 1)
