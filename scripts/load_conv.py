ba91a933e41147dc267b65114bfadf240e44c843
Matthew Speers <mattspeers33@gmail.com>
2023-05-05 11:28:40 +0100

    s

--- scripts/load_conv.py
+++ scripts/load_conv.py
@@ -10,16 +10,16 @@ env_probs[env_probs.p != 0]
 num_sea_states = 20
 z_values = np.linspace(-100, 50, 50)
 
-x_num = 100
-X = np.linspace(0, 2, num=x_num)
+x_num = 1000
+X = np.linspace(0, 20, num=x_num)
 
 np.random.seed(1)
 
-results = np.empty((env_probs.shape[0], x_num))
+cnv_results = np.empty((env_probs.shape[0], x_num))
 
-for i_s in range(env_probs.shape[0]):
-    print(i_s)
-    row = env_probs.loc[i_s]
+for s in range(env_probs.shape[0]):
+    print(s)
+    row = env_probs.loc[s]
     hs = np.tile(row['hs'], num_sea_states)
     s2 = np.tile(row['s2'], num_sea_states)
     tp = np.sqrt((hs*2*np.pi)/(s2*9.81))
@@ -32,11 +32,11 @@ for i_s in range(env_probs.shape[0]):
     loadEst.compute_sea_state_max()
     loadEst.compute_is_distribution(X=X)
 
-    results[i_s, :] = loadEst.cdf
+    cnv_results[s, :] = loadEst.cdf
 
-np.savetxt("scripts/results.csv", results, delimiter=",")
+np.savetxt("scripts/results.csv", cnv_results, delimiter=",")
 p_array = np.array(env_probs['p'])
-f_cdf = np.sum(results * p_array[:, np.newaxis], axis=0)
+f_cdf = np.sum(cnv_results * p_array[:, np.newaxis], axis=0)
 
 plt.figure()
 plt.subplot(2, 1, 1)
