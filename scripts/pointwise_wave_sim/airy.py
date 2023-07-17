e742fee003a6af843574c38804300d46c41783ce
Matthew Speers <mattspeers33@gmail.com>
2023-07-17 16:11:20 +0100

    parallesided conditional distribution estimation

---
+++ scripts/pointwise_wave_sim/airy.py
@@ -0,0 +1,23 @@
+import numpy as np
+from wavesim import kinematics as kin
+from wavesim import loading as load
+from wavesim import spectrum as spctr
+
+if __name__ == '__main__':
+
+    H = np.array([10])
+    T = np.array([10])
+    ss1 = spctr.SeaState(H_det=H, T_det=T)
+
+    z_num = 150
+    z_range = np.linspace(-100, 50, z_num)
+    freq = 4.00  # 3. / (2*np.pi)
+    period = 60  # total time range
+
+    airy_wave = kin.AiryKin(sample_f=freq, period=period, z_values=z_range, sea_state=ss1)
+    airy_wave.compute_kinematics()
+    airy_wave.plot_kinematics(s=0)
+
+    airy_load = load.MorisonLoad(airy_wave)
+    airy_load.compute_load()
+    airy_load.plot_load()
---
+++ scripts/response_sim/dist_write_parallel.py
@@ -0,0 +1,58 @@
+import multiprocessing as mp
+import numpy as np
+import pandas as pd
+import wavesim.distest as dist
+import wavesim.spectrum as spctr
+import pickle
+
+
+def save_object(obj, filename):
+    with open(filename, 'wb') as outp:  # Overwrites any existing file.
+        pickle.dump(obj, outp, pickle.HIGHEST_PROTOCOL)
+
+
+def compute_response_dist(s: list):
+
+    print(s)
+    row = env_probs.loc[s]
+    hs = np.tile(row['x'], num_sea_states)
+    s2 = np.tile(row['y'], num_sea_states)
+    tp = np.sqrt((hs*2*np.pi)/(s2*9.81))
+    ss = spctr.SeaState(hs=hs, tp=tp, spctr_type=spctr.Jonswap)
+
+    loadEst = dist.LoadDistEst(sea_state=ss, z_values=z_values)
+    loadEst.compute_cond_crests()
+    loadEst.compute_kinematics()
+    loadEst.compute_load()
+    loadEst.compute_sea_state_max()
+    loadEst.compute_is_distribution(X=X)
+    loadEst.compute_density()
+    loadEst.kinematics = 0
+    loadEst.max_series = 0
+    loadEst.load = 0
+
+    # save to vector
+    return loadEst
+
+
+if __name__ == '__main__':
+
+    env_probs = pd.read_csv('scripts/env_probs.csv')
+    env_probs = env_probs[env_probs.p != 0].reset_index()
+
+    num_sea_states = 2000
+    z_values = np.linspace(-100, 50, 50)
+
+    x_num = 1000
+    X = np.linspace(0, 20, num=x_num)
+
+    np.random.seed(1)
+
+    print(env_probs.shape[0])
+    cond_dists = []
+
+    cl = mp.Pool(4)
+    cond_dists = cl.map(compute_response_dist, [i for i in range(env_probs.shape[0])])
+
+    # pickle dump
+    save_object(cond_dists, 'scripts/cond_dists.pkl')
