a255714148f6a8f687d2c2e32461ef9d93ef0a12
Matthew Speers <97178743+speers26@users.noreply.github.com>
2024-05-03 14:51:27 +0100

    .

--- test_scripts/pointwise_wave_sim/linear.py
+++ test_scripts/pointwise_wave_sim/linear.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 a = np.array([25])
 depth = 100
 cond = False
-ss1 = spctr.SeaState(hs=np.array([15]), tp=np.array([10]), spctr_type=spctr.Jonswap)
+ss1 = spctr.SeaState(hs=np.array([15]), tp=np.array([10]), current=0.0, spctr_type=spctr.Jonswap)
 
 np.random.seed(1)
 
