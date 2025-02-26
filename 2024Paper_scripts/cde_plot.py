4ae217c833850142d401649843a6717d2eedc7d6
Matthew Speers <97178743+speers26@users.noreply.github.com>
2025-02-26 15:54:58 +0000

    plotting working

--- 2024Paper_scripts/cde_plot.py
+++ 2024Paper_scripts/cde_plot.py
@@ -7,13 +7,11 @@ env_probs = pd.read_csv('/home/speersm/GitHub/Linear-Waves-and-Base-Shear-Estima
 cde = pd.read_csv('/home/speersm/GitHub/Linear-Waves-and-Base-Shear-Estimation-/2024Paper_scripts/cond_dens_duo.csv', header=None)
 
 # plot heatmap of cde over env_probs x and y
-num_x = 180 
-num_y = 90
-cde = np.array(cde).reshape(num_x, num_y)
-x = np.array(env_probs['x']).reshape(num_x, num_y)
-y = np.array(env_probs['y']).reshape(num_x, num_y)
+cde = np.array(cde)
+x = np.array(env_probs['x'])
+y = np.array(env_probs['y'])
 
-plt.imshow(cde, extent=[x.min(), x.max(), y.min(), y.max()], origin='lower', aspect='auto', cmap='hot_r')
+plt.scatter(x, y, c=cde, cmap='hot_r', marker='s')
 plt.colorbar(label='CDE')
 plt.xlabel('Hs [m]')
 plt.ylabel('Tp [s]')
